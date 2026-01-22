a422f32cafed29550a16334e4d7d09b686e13a1f
xxx <enpunk90@gmail.com>
2026-01-23 01:45:38 +0900

    초기 데이터 setting

---
+++ src/main/resources/data.sql
@@ -0,0 +1,23 @@
+-- Team 데이터 삽입
+INSERT INTO team (name) VALUES ('Team A');
+INSERT INTO team (name) VALUES ('Team B');
+INSERT INTO team (name) VALUES ('Team C');
+INSERT INTO team (name) VALUES ('Team D');
+INSERT INTO team (name) VALUES ('Team E');
+INSERT INTO team (name) VALUES ('Team F');
+INSERT INTO team (name) VALUES ('Team G');
+INSERT INTO team (name) VALUES ('Team H');
+INSERT INTO team (name) VALUES ('Team I');
+INSERT INTO team (name) VALUES ('Team J');
+
+-- Member 데이터 삽입 (각 팀에 1명씩 배치)
+INSERT INTO member (name, team_id) VALUES ('Member 1', 1);
+INSERT INTO member (name, team_id) VALUES ('Member 2', 2);
+INSERT INTO member (name, team_id) VALUES ('Member 3', 3);
+INSERT INTO member (name, team_id) VALUES ('Member 4', 4);
+INSERT INTO member (name, team_id) VALUES ('Member 5', 5);
+INSERT INTO member (name, team_id) VALUES ('Member 6', 6);
+INSERT INTO member (name, team_id) VALUES ('Member 7', 7);
+INSERT INTO member (name, team_id) VALUES ('Member 8', 8);
+INSERT INTO member (name, team_id) VALUES ('Member 9', 9);
+INSERT INTO member (name, team_id) VALUES ('Member 10', 10);
