2afc9a01238ec499423f6b29f359b99cfee130fa
xxx <enpunk90@gmail.com>
2026-01-23 01:53:32 +0900

    Repository 추가

---
+++ src/main/java/com/example/nplusone/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.example.nplusone.repository;
+
+import com.example.nplusone.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
---
+++ src/main/java/com/example/nplusone/repository/TeamRepository.java
@@ -0,0 +1,9 @@
+package com.example.nplusone.repository;
+
+import com.example.nplusone.domain.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TeamRepository extends JpaRepository<Team, Long> {
+}
